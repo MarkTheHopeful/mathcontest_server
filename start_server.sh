ec0f82de0a362af72effa08f138de7f4bba26225
MarkTheHopeful <marik45452012@yandex.ru>
2020-07-09 15:34:30 +0300

    Base for server init script

---
+++ start_server.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# You should run this inside a tmux session
+
+python3 .base_server.py 2&> .base_server.log
+
+# And then leave the session using ^-B d
+# TODO: Make the whole server start process run using just one (this) script
