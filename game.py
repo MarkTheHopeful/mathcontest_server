f30a8486d11903bf0a30d132d4cc758b9acb7efd
MarkTheHopeful <marik45452012@yandex.ru>
2020-07-09 18:12:54 +0300

    Console mode is ready, game class is somehow done

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import sympy as sp
 import operators as ops
 from entities.Player import Player
 
+
 class Game:
     players = []
     turn_num = 0
@@ -17,6 +18,31 @@ class Game:
     def current_player(self):
         return self.players[self.turn_num % len(self.players)]
 
+    def current_opponent(self):
+        return self.players[(self.turn_num + 1) % len(self.players)]
+
+    def get_function(self, position):
+        player = self.current_player()
+        if position >= len(player.functions):
+            player = self.current_opponent()
+            position -= len(player.functions)
+        return player.functions[position]
+
+    def apply_operator(self, op_ind, fun_inds):
+        try:
+            player = self.current_player()
+            oper = player.operators[op_ind]
+            position = fun_inds[0]
+            arguments = [self.get_function(ind) for ind in fun_inds]
+            res_function = oper(*arguments)
+            if position >= len(player.functions):
+                position -= len(player.functions)
+                player = self.current_opponent()
+            player.functions[position] = res_function
+            return APPLY_SUCCESS
+        except Exception:  # TODO: This is very dumb
+            return APPLY_FAILED
+
 
 if __name__ == "__main__":
     x = sp.symbols("x")
@@ -30,22 +56,26 @@ if __name__ == "__main__":
 
     while True:
         player = main_game.current_player()
+        opponent = main_game.current_opponent()
         print(f"PLayer {player.name}, it's your turn now!")
-        print(f"Your functions is:")
+        print(f"Your functions are:")
         print(*player.functions, sep=", ")
+        print("Your opponent's function are:")
+        print(*opponent.functions, sep=", ")
         print(f"Your operators is:")
         print(*player.operators)
         position = int(input("Choose place to the new function! It will also be the first argument\n"))
-        args = [player.functions[position]]
-        arg_in = int(input("Enter the operator's index!\n"))
-        oper = player.operators[arg_in]
-        while len(args) < oper.ar_cnt:
+        args = [position]
+        op_ind = int(input("Enter the operator's index!\n"))
+        ar_cnt = player.operators[op_ind].ar_cnt
+        while len(args) < ar_cnt:
             new_pos = int(input(f"Enter the index of argument number {len(args) + 1}!\n"))
-            args.append(player.functions[new_pos])
-        print(f"The operator is: {oper}")
-        print(f"The arguments are:", *args, sep=", ")
-        result = oper(*args)
-        print(f"The result is: {result}")
-        player.functions[position] = result
-        turn = 1
-
+            args.append(new_pos)
+        print(f"The operator is: {player.operators[op_ind]}")
+        print(f"The arguments are:", *[main_game.get_function(pos) for pos in args], sep=", ")
+        apply_state = main_game.apply_operator(op_ind, args)
+        if apply_state == APPLY_SUCCESS:
+            print(f"The result is: {main_game.get_function(position)}")
+            main_game.turn_num += 1
+        else:
+            print("FATAL ERROR, idk why, please, try again, lol")
