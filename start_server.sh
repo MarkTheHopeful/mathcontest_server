5f1e43f8b28d1a407b692832e5e44f9353c5c0c0
MarkTheHopeful <marik45452012@yandex.ru>
2020-07-10 06:29:33 +0000

    Dumb. Fixed.

--- start_server.sh
+++ start_server.sh
@@ -2,7 +2,8 @@
 
 # You should run this inside a tmux session
 
-python3 .base_server.py 2&> .base_server.log
+python3 base_server.py 2&> .base_server.log
 
 # And then leave the session using ^-B d
 # TODO: Make the whole server start process run using just one (this) script
+
