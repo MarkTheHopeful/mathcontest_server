a46b35ab2da5105a7b35cb066270a59e4143a988
MarkTheHopeful <marik45452012@yandex.ru>
2020-07-13 17:14:12 +0300

    Host:Port fixed in start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,7 @@
 # You should run this inside a tmux session
 
 export FLASK_APP=mathcontest_server.py
-flask run &> .mathcontest_server.log
+flask run --host=0.0.0.0 --port=5660 &> .mathcontest_server.log
 
 # And then leave the session using ^-B d
 # TODO: Make the whole app start process run using just one (this) script
