767ccc67b0bfd4698017fe9530f3c8340bf8e0c6
MarkTheHopeful <marik45452012@yandex.ru>
2020-07-13 16:17:48 +0300

    im dumb, soryr

--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,7 @@
 # You should run this inside a tmux session
 
 export FLASK_APP=mathcontest_server.py
-flask run 2&> .mathcontest_server.log
+flask run &> .mathcontest_server.log
 
 # And then leave the session using ^-B d
 # TODO: Make the whole app start process run using just one (this) script
